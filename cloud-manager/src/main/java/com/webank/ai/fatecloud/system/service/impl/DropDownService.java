30cfa70567c8b6da3763490c6daad4c62cee81c3
v_wbymxu <g6xuyiming@163.com>
2020-10-12 16:08:21 +0800

    fix bug of drop-down list
    Signed-off-by: v_wbymxu <g6xuyiming@163.com>

--- cloud-manager/src/main/java/com/webank/ai/fatecloud/system/service/impl/DropDownService.java
+++ cloud-manager/src/main/java/com/webank/ai/fatecloud/system/service/impl/DropDownService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.webank.ai.fatecloud.system.dao.entity.FederatedSiteManagerDo;
 import com.webank.ai.fatecloud.system.dao.mapper.FederatedSiteManagerMapper;
 import com.webank.ai.fatecloud.system.pojo.qo.DropDownVersionQo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,18 +18,24 @@ public class DropDownService {
 
     public List<String> getDropDownVersionList(DropDownVersionQo dropDownVersionQo) {
         QueryWrapper<FederatedSiteManagerDo> federatedSiteManagerDoQueryWrapper = new QueryWrapper<>();
-        federatedSiteManagerDoQueryWrapper.select(dropDownVersionQo.getVersionName()).eq("institutions",dropDownVersionQo.getInstitutions()).groupBy(dropDownVersionQo.getVersionName());
+        federatedSiteManagerDoQueryWrapper.select(dropDownVersionQo.getVersionName()).eq("institutions", dropDownVersionQo.getInstitutions()).groupBy(dropDownVersionQo.getVersionName());
 
         List<FederatedSiteManagerDo> federatedSiteManagerDos = federatedSiteManagerMapper.selectList(federatedSiteManagerDoQueryWrapper);
         ArrayList<String> versionList = new ArrayList<>();
         for (FederatedSiteManagerDo federatedSiteManagerDo : federatedSiteManagerDos) {
-            if(federatedSiteManagerDo==null){
+            if (federatedSiteManagerDo == null) {
                 continue;
             }
             if ("fate_version".equals(dropDownVersionQo.getVersionName())) {
+                if (StringUtils.isBlank(federatedSiteManagerDo.getFateVersion())) {
+                    continue;
+                }
                 versionList.add(federatedSiteManagerDo.getFateVersion());
             }
             if ("fate_serving_version".equals(dropDownVersionQo.getVersionName())) {
+                if (StringUtils.isBlank(federatedSiteManagerDo.getFateServingVersion())) {
+                    continue;
+                }
                 versionList.add(federatedSiteManagerDo.getFateServingVersion());
             }
         }
