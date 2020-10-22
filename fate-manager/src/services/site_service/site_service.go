ab5aa05ae3a33f18f4de0ab4433d86f154f33c3f
nobslin <709357785@qq.com>
2020-10-22 19:09:32 +0800

    update web js

--- fate-manager/src/services/site_service/site_service.go
+++ fate-manager/src/services/site_service/site_service.go
@@ -91,6 +91,17 @@ func GetHomeSiteList() ([]*entity.FederatedItem, error) {
 		var federatedItem entity.FederatedItem
 		federatedItem.FederatedOrganization = federatedSiteList[0].FederatedOrganization
 		federatedItem.Institutions = federatedSiteList[0].Institutions
+		accountInfo := models.AccountInfo{
+			Role:   int(enum.UserRole_ADMIN),
+			Status: int(enum.IS_VALID_YES),
+		}
+		accountInfoList, err := models.GetInstitution(accountInfo)
+		if err != nil || len(accountInfoList) == 0 {
+			logging.Error("admin user not exists!")
+			return nil, err
+		}
+
+		federatedItem.FateManagerInstitutions = accountInfo.Institutions
 		fedetatedMap[federatedSiteList[0].FederatedId] = &federatedItem
 	} else {
 		for i := 0; i < len(federatedSiteList); i++ {
