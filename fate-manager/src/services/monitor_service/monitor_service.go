9fbb9a6b85dfb0085c143c07bc571ff86cbbe88f
nobslin <709357785@qq.com>
2020-10-26 19:36:23 +0800

    add  1.1 for ansible

---
+++ fate-manager/src/services/monitor_service/monitor_service.go
@@ -0,0 +1,15 @@
+package monitor_service
+
+import "fate.manager/entity"
+
+func GetMonitorTotal()(*entity.MonitorTotalResp,error){
+
+}
+
+func GetInstitutionBaseStatics()([]entity.InstitutionBaseStaticsResp,error){
+
+}
+
+func GetSiteBaseStatistics()([]entity.SiteBaseStatisticsResp,error){
+
+}
