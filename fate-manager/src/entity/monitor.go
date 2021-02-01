d1b77be0f137a46fa307a561ff7890dd100385a3
nobslin <709357785@qq.com>
2021-02-01 10:51:06 +0800

    add

--- fate-manager/src/entity/monitor.go
+++ fate-manager/src/entity/monitor.go
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 The FATE Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package entity
 
 type JobBase struct {
@@ -32,7 +47,7 @@ type InstitutionModelingItem struct {
 type InstitutionBaseStaticsResp struct {
 	JobBase
 	InstitutionModeling []InstitutionModelingItem `json:"data"`
-	Total int `json:"total"`
+	Total               int                       `json:"total"`
 }
 type MixSiteModeling struct {
 	SiteName string `json:"siteName"`
@@ -47,7 +62,7 @@ type InstitutionSiteModelingItem struct {
 	InstitutionSiteList []InstitutionSite `json:"institutionSiteList"`
 }
 type InsitutionSiteModeling struct {
-	SiteList []string `json:"siteList"`
+	SiteList      []string                      `json:"siteList"`
 	OtherSiteList []InstitutionSiteModelingItem `json:"data"`
-	Total int `json:"total"`
+	Total         int                           `json:"total"`
 }
